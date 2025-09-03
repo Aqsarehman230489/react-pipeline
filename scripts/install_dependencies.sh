c97431daa8ce30d30ecd869f04bb2cc863f146f0
aqsa.rehman <aqsarehman373@gmail.com>
2025-09-03 17:33:15 +0500

    updated install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,16 +3,22 @@ set -e
 
 echo "=== Installing Docker on Amazon Linux ==="
 
+# Update the system to get the latest package information
+sudo dnf update -y
+
 # Install Docker
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
+sudo dnf install -y docker
+
+# Start the Docker service
+sudo systemctl start docker
 sudo systemctl enable docker
 
 # Add ec2-user to docker group
 sudo usermod -a -G docker ec2-user
 
 # Install Docker Compose
+# Docker Compose is now officially a plugin for Docker and uses a different install method
+# The method below is for the standalone v2.x version
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
