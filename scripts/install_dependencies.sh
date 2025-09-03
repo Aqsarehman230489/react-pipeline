d5df404437c38a15f3fd7f8619f0653cfa7edeff
aqsa.rehman <aqsarehman373@gmail.com>
2025-09-03 18:02:57 +0500

    updated install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -21,6 +21,6 @@ sudo usermod -a -G docker ec2-user
 # The method below is for the standalone v2.x version
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 echo "=== Docker setup completed ==="
